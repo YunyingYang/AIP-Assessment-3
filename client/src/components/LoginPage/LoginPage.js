2146ba35037b24584ea79bd3ecc9817babeb1c2a
YunyingYang <12428898@student.uts.edu.au>
2018-09-21 20:18:19 +1000

    add movie poster img when search movie

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,127 +1,158 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import { Link } from 'react-router-dom';
-import Background from '../../images/usermgmtbg.jpg';
-import cross from '../../images/cross.png';
-import { connect } from 'react-redux';
-import { loginUser } from '../../actions/authActions';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { Link } from "react-router-dom";
+import Background from "../../images/usermgmtbg.jpg";
+import cross from "../../images/cross.png";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
 
 var sectionStyle = {
-    width: "600px",
-    height: "410px",
-    border: "3px solid #000",
-    borderRadius: "20px 70px",
-    // makesure here is String, following is ES6
-    backgroundImage: `url(${Background})`
+  width: "600px",
+  height: "410px",
+  border: "3px solid #000",
+  borderRadius: "20px 70px",
+  // makesure here is String, following is ES6
+  backgroundImage: `url(${Background})`
 };
 
 class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
 
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {}
-        };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
+  }
 
-    componentDidMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
-        if (nextProps.errors) {
-            this.setState({ errors: nextProps.errors });
-        }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
+  }
 
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
-    }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    onSubmit(e) {
-        e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        }
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
 
-        this.props.loginUser(userData);
-    }
+    this.props.loginUser(userData);
+  }
 
-    render() {
-        const { errors } = this.state;
-        return (
-            <div className="login">
-                <div className="container" style={sectionStyle}>
-                    <Link to='/'><img src={cross} style={{ width: '30px', height: '30px', float: 'left', padding: '2px 2px' }} /></Link>
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <br />
-                            <br />
-                            <h1 className="display-4 text-center" style={{ color: "#FFF", textShadow: "0 2px #FF7F00, 2px 0 #FF7F00, -2px 0 #FF7F00, 0 -2px #FF7F00" }}>
-                                User Login
-                            </h1>
-                            {/* <p className = "lead text-center">
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="login">
+        <div className="container" style={sectionStyle}>
+          <Link to="/">
+            <img
+              src={cross}
+              style={{
+                width: "30px",
+                height: "30px",
+                float: "left",
+                padding: "2px 2px"
+              }}
+            />
+          </Link>
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <br />
+              <h1
+                className="display-4 text-center"
+                style={{
+                  color: "#FFF",
+                  textShadow:
+                    "0 2px #FF7F00, 2px 0 #FF7F00, -2px 0 #FF7F00, 0 -2px #FF7F00"
+                }}
+              >
+                User Login
+              </h1>
+              {/* <p className = "lead text-center">
                                 Log in with your Filmtopia account!
                             </p> */}
-                            <br />
-                            <form noValidate onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <input type="email"
-                                        className={classnames('form-control form-control-lg', {
-                                            'is-invalid': errors.email
-                                        })}
-                                        placeholder="Email Address"
-                                        name="email"
-                                        value={this.state.email}
-                                        onChange={this.onChange}
-                                        required />
-                                    {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-                                </div>
-                                <div className="form-group">
-                                    <input type="password"
-                                        className={classnames('form-control form-control-lg', {
-                                            'is-invalid': errors.password
-                                        })}
-                                        placeholder="Password"
-                                        name="password"
-                                        value={this.state.password}
-                                        onChange={this.onChange}
-                                        required />
-                                    {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-                                </div>
-                                <br />
-                                <input type="submit" className="btn btn-primary" value="Sign In" />
-                            </form>
-                        </div>
-                    </div>
+              <br />
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    required
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    required
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
+                </div>
+                <br />
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Sign In"
+                />
+              </form>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 LoginPage.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+  auth: state.auth,
+  errors: state.errors
 });
 
-export default connect(mapStateToProps, { loginUser })(LoginPage);
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(LoginPage);
--- client/src/components/MovieSearch/movieItem.js
+++ client/src/components/MovieSearch/movieItem.js
@@ -1,29 +1,69 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Router, Route, Switch, Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-// import isEmpty from '../../validation/is-empty';
+import { connect } from "react-redux";
+import "./movieSearch.css";
 
 class MovieItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tmdbResult: {}
+    };
+  }
+
+  componentDidMount() {
+    const { movie } = this.props;
+    const url = new URL(
+      "https://api.themoviedb.org/3/movie/" +
+        movie.tmdbId +
+        "?api_key=9ff347d908a575c777ebecebe3fdcf6b&language=en-US"
+    );
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ tmdbResult: res.data });
+      })
+      .catch(err => this.setState(console.log("cannot find from tmdb")));
+  }
+
   render() {
     const { movie } = this.props;
 
-    return (
-      <div className="card w-75 border-info mb-3" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">{movie.title}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{movie.genres}</h6>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+    const picBaseUrl = new URL(
+      "http://image.tmdb.org/t/p/w185_and_h278_bestv2/"
+    );
 
-          {/* <a href="#" className="card-link">
+    return (
+      <div className="card w-75 border-warning mb-3" style={{ width: "18rem" }}>
+        <tr>
+          <th>
+            <img
+              className="card-img-left pic_size1"
+              src={picBaseUrl + this.state.tmdbResult.poster_path}
+              alt="Card image cap"
+            />
+          </th>
+          <th>
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{movie.genres}</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+
+              {/* <a href="#" className="card-link">
             Card link
           </a>
           <a href="#" className="card-link">
             Another link
           </a> */}
-        </div>
+            </div>
+          </th>
+        </tr>
       </div>
     );
   }
@@ -33,4 +73,11 @@ MovieItem.propTypes = {
   movie: PropTypes.object.isRequired
 };
 
-export default MovieItem;
+const mapStateToProps = state => ({
+  // auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(withRouter(MovieItem));
