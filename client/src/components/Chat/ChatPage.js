8115e9c139aae48d7630028fab782c23a15545cc
900MillionBoysDream <Jiayi.Shi@student.uts.edu.au>
2018-10-03 01:09:23 +1000

    Change chat UI

--- client/src/components/Chat/ChatPage.js
+++ client/src/components/Chat/ChatPage.js
@@ -5,16 +5,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import "./ChatPage.css";
-import Background from "../../images/chatbg.jpg";
-
-var sectionStyle = {
-  width: "1000px",
-  height: "600px",
-  border: "5px solid #FFF",
-  borderRadius: "20px 70px",
-  // makesure here is String, following is ES6
-  backgroundImage: `url(${Background})`
-};
 
 class ChatPage extends Component {
   constructor(props) {
@@ -48,67 +38,76 @@ class ChatPage extends Component {
   render() {
     const { user } = this.props.auth;
     return (
-      <div className="container" style={sectionStyle}>
-        <div className="row">
-          <div className="col-sm-12" style={{ padding: "10px" }}>
-            <div>
+      <div className="chat-page">
+        <div
+          className="container alert alert-dismissible alert-secondary"
+          style={{ maxWidth: "60rem" }}
+        >
+          <div className="row">
+            <div className="col-sm-12" style={{ padding: "10px" }}>
               <div>
-                <h2>Let's share feelings about movies!</h2>
-                <hr />
-                <div
-                  className="messages"
-                  style={{
-                    width: "100%",
-                    maxHeight: "350px",
-                    overflowY: "auto"
-                  }}
-                >
-                  {this.state.messages.map((message, index) => {
-                    return (
-                      <p
-                        className={
-                          this.state.username === message.author
-                            ? "alert alert-dismissible alert-success"
-                            : "alert alert-dismissible alert-info"
-                        }
-                        key={index}
-                        style={{ wordBreak: "break-all" }}
-                      >
-                        <strong>{message.author}:</strong>
-                        &nbsp;
-                        {message.message}
-                      </p>
-                    );
-                  })}
+                <div>
+                  <h2>Let's share feelings about movies!</h2>
+                  <hr />
+                  <div
+                    className="messages"
+                    style={{
+                      width: "100%",
+                      maxHeight: "350px",
+                      overflowY: "auto"
+                    }}
+                  >
+                    {this.state.messages.map((message, index) => {
+                      return (
+                        <p
+                          className={
+                            this.state.username === message.author
+                              ? "alert alert-dismissible alert-success"
+                              : "alert alert-dismissible alert-info"
+                          }
+                          key={index}
+                          style={{ wordBreak: "break-all" }}
+                        >
+                          <strong>{message.author}:</strong>
+                          &nbsp;
+                          {message.message}
+                        </p>
+                      );
+                    })}
+                  </div>
+                  <hr />
                 </div>
-                <hr />
-              </div>
-              <div>
-                <form onSubmit={this.sendMessage}>
-                  {/* <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({ username: ev.target.value })} className="form-control" />
+                <div>
+                  <form onSubmit={this.sendMessage}>
+                    {/* <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({ username: ev.target.value })} className="form-control" />
                                 <br /> */}
-                  <input
-                    type="text"
-                    placeholder="Message"
-                    className="form-control"
-                    required
-                    value={this.state.message}
-                    onChange={ev =>
-                      this.setState({
-                        username: user.name,
-                        message: ev.target.value
-                      })
-                    }
-                  />
-                  <br />
-                  <button type="submit" className="btn btn-primary">
-                    Send
-                  </button>
-                </form>
+                    <input
+                      type="text"
+                      placeholder="Message"
+                      className="form-control"
+                      required
+                      value={this.state.message}
+                      onChange={ev =>
+                        this.setState({
+                          username: user.name,
+                          message: ev.target.value
+                        })
+                      }
+                    />
+                    <br />
+                    <button type="submit" className="btn btn-secondary">
+                      Send
+                    </button>
+                    <br />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <br />
+        <br />
+        <br />
       </div>
     );
   }
