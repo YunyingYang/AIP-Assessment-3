bd6eb6b58fc9c3de88e78a2a270c0ebd907db702
900MillionBoysDream <Jiayi.Shi@student.uts.edu.au>
2018-10-09 19:58:09 +1100

    Maybe fix mystery bug

--- server/validation/profile.js
+++ server/validation/profile.js
@@ -1,20 +1,20 @@
-const Validator = require('validator');
-const isEmpty = require('./isempty');
+const Validator = require("validator");
+const isEmpty = require("./isempty");
 
 module.exports = function validateProfileInput(data) {
   let errors = {};
 
-  data.handle = !isEmpty(data.handle) ? data.handle : '';
-  data.status = !isEmpty(data.status) ? data.status : '';
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.status = !isEmpty(data.status) ? data.status : "";
 
   if (!Validator.isLength(data.handle, { min: 2, max: 40 }))
-    errors.handle = 'Handle needs to between 2 and 4 characters';
+    errors.handle = "Handle needs to between 2 and 4 characters";
 
   if (Validator.isEmpty(data.handle))
-    errors.handle = 'Profile handle is required';
+    errors.handle = "Profile handle is required";
 
-  if (Validator.isEmpty(data.status))
-    errors.status = 'Status field is required';
+  if (Validator.isEmpty(data.status) || data.status === 0)
+    errors.status = "Status field is required";
 
   return {
     errors,
